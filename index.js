8628dbc42b17a3c8a450eba4d4cc2a1490a7b31c
Karl Westin <karl.westin@gmail.com>
2017-08-22 14:22:42 +0200

    feat: use different wave form options

--- index.js
+++ index.js
@@ -3,21 +3,64 @@
  * generates raw PCM data for a tone,
  * specify frequency, length, volume and sampling rate
  */
-function generateCycle(cycle, volume) {
+
+var shapes = {
+  sine: function (i, cycle, volume) {
+    // i / cycle => value between 0 and 1
+    // 0 = beginning of circly
+    // 0.25 Math.sin = 1
+    // 0.5 Math.sin = 0
+    // 0.75 Math.sin = -1
+    // 1 Math.sin = 1
+    return Math.min(volume * Math.sin((i/cycle) * Math.PI * 2), volume - 1);
+  },
+  triangle: function (i, cycle, volume) {
+    var halfCycle = cycle / 2
+    var level
+
+    if (i < halfCycle) {
+      level = (volume * 2) * (i / halfCycle) - volume;
+    } else {
+      i = i - halfCycle
+      level = -(volume * 2) * (i / halfCycle) + volume;
+    }
+
+    return Math.min(level, volume - 1);
+  },
+  saw: function (i, cycle, volume) {
+    return Math.min((volume * 2) * (i / cycle) - volume, volume - 1);
+  },
+  square: function (i, cycle, volume) {
+    if(i > cycle / 2) {
+      return volume - 1;
+    }
+
+    return -volume;
+  }
+}
+
+function generateCycle(cycle, volume, shape) {
   var data = [];
   var tmp;
+  var generator = typeof shape == 'function' ? shape : shapes[shape];
+  if (!generator) {
+    throw new Error('Invalid wave form: "' + shape + '" choose between: ' + Object.keys(shapes));
+  }
+
   for(var i = 0; i < cycle; i++) {
-    tmp = Math.min(volume * Math.sin((i/cycle) * Math.PI * 2), volume - 1)
+    tmp = generator(i, cycle, volume);
     data[i] = Math.round(tmp);
   }
   return data;
 }
 
-module.exports = function(freq, lengthInSecs, volume, rate) {
-  freq = freq || 440;
-  rate = rate || 44100
-  lengthInSecs = lengthInSecs || 2.0;
-  volume = volume || 30
+module.exports = function(opts) {
+  opts = opts || {}
+  var freq = opts.freq || 440;
+  var rate = opts.rate || 44100
+  var lengthInSecs = opts.lengthInSecs || 2.0;
+  var volume = opts.volume || 30;
+  var shape = opts.shape || 'sine';
 
   var cycle = Math.floor(rate/freq);
   var samplesLeft = lengthInSecs * rate;
@@ -25,7 +68,7 @@ module.exports = function(freq, lengthInSecs, volume, rate) {
   var ret = [];
 
   for(var i = 0; i < cycles; i++) {
-    ret = ret.concat(generateCycle(cycle, volume));
+    ret = ret.concat(generateCycle(cycle, volume, shape));
   }
 
   return ret;
--- test.js
+++ test.js
@@ -4,10 +4,12 @@ var tonegenerator = require('./')
 console.log('Testing Tonegenerator...')
 
 
-var tone1 = tonegenerator(440, 2, 10)
-var tone2 = tonegenerator(440, 2, 30)
-var tonefrequency = tonegenerator(440, 2, 10, 22050)
+var tone1 = tonegenerator({ freq: 440, lengthInSecs: 2, volume: 10 })
+var tone2 = tonegenerator({ freq: 440, lengthInSecs: 2, volume: 30 })
+var tonefrequency = tonegenerator({ freq: 440, lengthInSecs: 2, volume: 10, rate: 22050 })
 
+
+console.log('Testing Sine Waves')
 assert(Array.isArray(tone1), 'Data is an array')
 
 // takes the volume argument - 1 as max
@@ -18,5 +20,54 @@ assert.strictEqual(Math.min.apply(Math, tone1), -10)
 assert.strictEqual(Math.max.apply(Math, tone2), 29)
 
 assert.equal(tone1.length/2, tonefrequency.length, 'when halving audio sampling rate, the array length should be half of default')
+
+
+console.log('Testing Triangle Waves')
+var tone4 = tonegenerator({ freq: 440, lengthInSecs: 2, volume: 10, shape: 'triangle' })
+// test that max is correct
+assert.strictEqual(Math.max.apply(Math, tone4), 9)
+// test that min is correct
+assert.strictEqual(Math.min.apply(Math, tone4), -10)
+// test that there are intermediate values
+assert(tone4.includes(0))
+assert(tone4.includes(-1))
+
+
+console.log('Testing Saw Waves')
+var tone5 = tonegenerator({ freq: 440, lengthInSecs: 2, volume: 10, shape: 'saw' })
+// test first val, should be min
+assert.strictEqual(tone5.shift(), -10)
+// test last val, should be max
+assert.strictEqual(tone5.pop(), 9)
+// test that there's intermediate values generated
+assert(tone5.includes(0))
+
+
+console.log('Testing Square Waves')
+var tone6 = tonegenerator({ freq: 440, lengthInSecs: 2, volume: 10, shape: 'square' })
+// test first val
+assert.strictEqual(tone6.pop(), 9)
+// test last val
+assert.strictEqual(tone6.shift(), -10)
+// make sure there's no intermediate values
+assert(!tone6.includes(0))
+
+
+console.log('Testing Custom Waves')
+var tone7 = tonegenerator({
+  freq: 440,
+  lengthInSecs: 2,
+  volume: 11,
+  shape: function (i, cycle, volume) {
+    return volume - 1;
+  }
+})
+// test first val
+assert.strictEqual(tone7.pop(), 10)
+// test last val
+assert.strictEqual(tone7.shift(), 10)
+// make sure there's no intermediate values
+assert(!tone7.includes(0))
+
 
 console.log('...done')
